01083b425948f812e562e04446ec4cb2f667e384
ttnclinton <ttnclinton@gmail.com>
2024-01-21 15:57:03 -0700

    select a question done

--- scripts/jr1.js
+++ scripts/jr1.js
@@ -7,19 +7,39 @@ let Guess = document.querySelector(".Guess");
 let Pass = document.querySelector(".Pass");
 let NextRound = document.querySelector(".NextRound");
 
-
+// grabbed for start the game
 function turn(){
-playerTurn.textContent="Player 1"
-};
-turn();
+    playerTurn.textContent="Player 1"
+    };
+    turn();
+    
+    Guess.disabled = true;
+    Pass.disabled = true;
+    NextRound.disabled = true;
 
-Guess.disabled = true;
-Pass.disabled = true;
-NextRound.disabled = true;
+// Select a Question
+// Grabbing Category
+let text1cate = document.querySelector(".text1.cate");
+let text1cell = document.querySelector(".text1.cell");
 
-// Select a question
 
-//placeHolder[1].question
+text1cate.textContent= placeholderQuestions[0].category;
 
-let 
+function text1CellClicked(e){
+    text1cell.textContent= placeholderQuestions[0].question; 
+    Guess.disabled = false;
+    Pass.disabled = false;  
+}
+
+text1cell.onclick=text1CellClicked;
+
+function passButtonClicked(){
+    playerTurn.textContent="Player 2";
+}
+
+Pass.onclick=passButtonClicked;
+
+
+
+ 
    
